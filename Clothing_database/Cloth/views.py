3d53d16a43e204a59968dcef9bb8b2b0eeadfee6
feruzxz <151874435+feruzxz@users.noreply.github.com>
2023-12-02 02:05:28 +0600

    view.py

--- Clothing_database/Cloth/views.py
+++ Clothing_database/Cloth/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 
 
+def home(request):
+    return render(request, 'index.html')
 def login_view(request):
     if request.method == 'POST':
         username = request.POST['username']
