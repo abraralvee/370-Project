8f1498972ee6d3b4e81c28ae9cc46b2581b3dce0
Maisha Tasnim <maishazm16@gmail.com>
2023-12-01 22:25:50 +0600

    models file

--- Clothing_database/Cloth/models.py
+++ Clothing_database/Cloth/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    User_ID = models.Model(max_length=10, primary_key=True)
+    User_ID = models.CharField(max_length=10, primary_key=True)
     First_name = models.CharField(max_length=50)
     Last_name = models.CharField(max_length=50)
     Phone_number= models.CharField(max_length=15)
@@ -21,18 +21,21 @@ class Rentee(models.Model):
 class Delivery_person(models.Model):
     User_ID = models.ForeignKey(User, on_delete = models.CASCADE, to_field = 'User_ID')
 
-class Clothing_item(models.Model):
+class Review(models.Model):
+    Serial_no = models.ForeignKey('ClothingItem', on_delete=models.CASCADE, to_field='Serial_no')
+    Reviews = models.CharField(max_length=50)
+
+class ClothingItem(models.Model):
     Serial_no = models.CharField(max_length=12, primary_key=True)
     Type = models.CharField(max_length=10)
     Condition = models.CharField(max_length=10)
-    Size = models.IntegerField(max_length=10)
+    Size = models.IntegerField()
     Category = models.CharField(max_length=10)
     Rent_status = models.CharField(max_length=10)
     Gender = models.CharField(max_length=10)
     Image = models.CharField(max_length=50, null=True)
-    Rent_status = models.CharField(max_length=10)
     Price = models.DecimalField(max_digits=10, decimal_places=2)
-    Reviews = models.ManyToOneRel('Review', related_name= 'Clothing_item')
+    Reviews = models.ManyToManyField(Review, related_name='clothing_items')
 
 class Cart(models.Model):
     User_ID = models.ForeignKey(User, on_delete = models.CASCADE, to_field = 'User_ID')
@@ -41,7 +44,7 @@ class Cart(models.Model):
     Daily_charge= models.DecimalField(max_digits=10, decimal_places=2)
     Delivery_charge = models.DecimalField(max_digits=10, decimal_places=2)
     Duration = models.DurationField()
-    Cart_number = models.
+    Cart_number = models.DecimalField(max_digits=10, decimal_places=2)
     Product_price = models.DecimalField(max_digits=10, decimal_places=2)
 
 class Transaction(models.Model):
@@ -50,14 +53,14 @@ class Transaction(models.Model):
 
 class Bkash(models.Model):
     Transaction_iD = models.ForeignKey(Transaction, on_delete = models.CASCADE, to_field = 'Transaction_iD')
-    Phone_no = models.IntegerField(max_length=15)
+    Phone_no = models.CharField(max_length=15)
 
 class Nagad(models.Model):
     Transaction_iD = models.ForeignKey(Transaction, on_delete = models.CASCADE, to_field = 'Transaction_iD')
-    Phone_no = models.IntegerField(max_length=15)
+    Phone_no = models.CharField(max_length=15)
 
 class Visa_card(models.Model):
     Transaction_iD = models.ForeignKey(Transaction, on_delete = models.CASCADE, to_field = 'Transaction_iD')
     First_name = models.CharField(max_length=50)
     Last_name = models.CharField(max_length=50)
-    Card_number = models.IntegerField(max_length=15)
+    Card_number = models.IntegerField()
