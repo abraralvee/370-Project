5f027eddaef68ce1553485c748d69e89d1ec3d9f
Maisha Tasnim <maishazm16@gmail.com>
2023-12-04 22:46:01 +0600

    LAST

--- Clothing_database/Cloth/views.py
+++ Clothing_database/Cloth/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from .forms import RenteeRegistrationForm, RenterRegistrationForm, DeliveryPersonRegistrationForm
-
+from django.db import connection
+from django.contrib import messages
 # Assuming you have a custom user model
 from .models import User
 
@@ -24,19 +25,32 @@ def register_rentee(request):
     return render(request, 'register_rentee.html', {'form': form})
 
 def register_renter(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = RenterRegistrationForm(request.POST)
+
         if form.is_valid():
             user = form.save(commit=False)
             user.Role = '02'
             user.save()
 
-            # Log in the user after registration
-            login(request, user)
+            # Now you can access user attributes like user.user_id, user.first_name, etc.
 
-            return redirect('success_page')  # Redirect to a success page
+            insert_renter = """
+                INSERT INTO Renter (user_id, SSN, Rating, address, email)
+                VALUES (%s, %s, %s, %s, %s)
+            """
+
+            with connection.cursor() as cursor:
+                cursor.execute(insert_renter, (user.user_id, form.cleaned_data['SSN'], None, form.cleaned_data['address'], form.cleaned_data['email']))
+            print(SSN)
+            messages.success(request, 'Signup Successful')
+            return redirect('../')  # Redirect to the user's profile page or any other desired page
+        else:
+            messages.warning(request, 'Please fix the errors in the form.')
+
     else:
         form = RenterRegistrationForm()
+
     return render(request, 'register_renter.html', {'form': form})
 
 def register_delivery_person(request):
