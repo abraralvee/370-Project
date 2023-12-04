1613e3d056a99cbec9a030efe455dad474efc6ee
Maisha Tasnim <maishazm16@gmail.com>
2023-12-04 21:10:07 +0600

    few changes

--- Clothing_database/Cloth/urls.py
+++ Clothing_database/Cloth/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.home, name='homepage'),
     path('register_renter/', views.register_renter, name='Renter Registration'),
     path('register_rentee/', views.register_rentee, name='Rentee Registration'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', views.login_view, name='login'),
+    path('renter-dashboard/',views.renter_dashboard,name='Renter dashboard')
+    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- Clothing_database/Cloth/views.py
+++ Clothing_database/Cloth/views.py
@@ -60,16 +60,7 @@ def home(request):
 
 
 def login_view(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            # Redirect to a success page.
-            return redirect('index')  # Change 'index' to the name of your home page URL
-        else:
-            # Return an 'invalid login' error message.
-            return render(request, 'login.html', {'error': 'Invalid login credentials'})
-
     return render(request, 'login.html')
+
+def renter_dashboard(request):
+    return render(request, 'renter-dashboard.html')
